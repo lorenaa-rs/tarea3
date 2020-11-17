f2609777ae8dbeda9ceb2524f21026de9b483d02
juanpabloacerom <juan.acero04@uptc.edu.co>
2020-11-17 09:06:49 -0500

    Agregué el servicio de la lista

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
     res.send('parque loco')
   })
 
+  let users = [
+      {name: "Lorena", age: 23, phone: "310863053"},
+      {name: "Juan", age: 25, phone: "3202386430"}      
+  ]
+
+  app.get('/users', (req, res) => {
+      res.send(users)
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
